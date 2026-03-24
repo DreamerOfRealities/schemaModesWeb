4b2767418e32cacfa879fa871a0bdfc4118e3f5b
DreamerOfRealities <dreamerofrealities@proton.me>
2026-03-24 22:20:02 +0100

    create barebone local storage functionality

--- script/texteditorScript.js
+++ script/texteditorScript.js
@@ -1,3 +1,36 @@
-var message = 'Hello World';
-console.log(message);
+var aHtmlElement = /** @class */ (function () {
+    function aHtmlElement(myId) {
+        this.cont = "Schreibe hier über dich...";
+        this.id = myId;
+        this.elem = document.getElementById(this.id);
+        if (!localStorage.getItem(this.id)) //if no local data
+            if (this.elem)
+                this.elem.textContent = this.cont; //set to current content
+            else if (this.elem)
+                this.elem.textContent = localStorage.getItem(this.id); //set to existing content
+        if (this.elem)
+            this.elem.addEventListener("input", function () {
+                localStorage.setItem(this.id, this.textContent);
+            });
+    }
+    return aHtmlElement;
+}());
+// Check for webStorage support
+if (typeof (Storage) == undefined) {
+    //if (!StorageManager.persist()){
+    console.log("webStorage not supported");
+    var descMain = document.getElementById("desc-main");
+    if (descMain)
+        descMain.textContent += " " + "Leider unterstützt dein Web-Browser kein webStorage, das heißt dein eingegebener Text wird nicht gespeichert.";
+    //throw new Error("Web storage unsupported")
+}
+var daText = new aHtmlElement("da-text");
+var ceText = new aHtmlElement("ce-text");
+// const daText: HTMLElement = document.getElementById("da-text")
+// if(!localStorage.getItem("da-text"))
+//     if(daText) daText.textContent = "Füge hier deine Beschreibung ein..."
+// else
+//     if(daText) daText.textContent = localStorage.getItem("da-text")
+// //if(daText) daText.onchange = localStorage.setItem( "da-text", daText.textContent)
+// if(daText) daText.addEventListener("input", function(){localStorage.setItem("da-text", daText.textContent)})
 //# sourceMappingURL=texteditorScript.js.map
--- script/texteditorScript.ts
+++ script/texteditorScript.ts
@@ -1,2 +1,41 @@
-let message: string = 'Hello World';
-console.log(message);
+
+
+class aHtmlElement{
+    id: string //must be equal to id of HTMLElement
+    elem: HTMLElement
+    cont: string = "Schreibe hier über dich..."
+
+    constructor(myId:string){
+        this.id = myId
+        this.elem = document.getElementById(this.id)
+        if(!localStorage.getItem(this.id)) //if no local data
+            if(this.elem) this.elem.textContent = this.cont //set to current content
+        else
+            if(this.elem) this.elem.textContent = localStorage.getItem(this.id) //set to existing content
+        if(this.elem) this.elem.addEventListener("input", function(){ //store changes made by input
+            localStorage.setItem(this.id, this.textContent)
+        })
+    }
+}
+
+// Check for webStorage support
+if (typeof(Storage)==undefined){
+//if (!StorageManager.persist()){
+    console.log("webStorage not supported")
+    const descMain = document.getElementById("desc-main")
+    if(descMain) descMain.textContent += " " + "Leider unterstützt dein Web-Browser kein webStorage, das heißt dein eingegebener Text wird nicht gespeichert."
+    //throw new Error("Web storage unsupported")
+}
+
+const daText = new aHtmlElement("da-text")
+const ceText = new aHtmlElement("ce-text")
+
+// const daText: HTMLElement = document.getElementById("da-text")
+// if(!localStorage.getItem("da-text"))
+//     if(daText) daText.textContent = "Füge hier deine Beschreibung ein..."
+// else
+//     if(daText) daText.textContent = localStorage.getItem("da-text")
+// //if(daText) daText.onchange = localStorage.setItem( "da-text", daText.textContent)
+// if(daText) daText.addEventListener("input", function(){localStorage.setItem("da-text", daText.textContent)})
+
+    
