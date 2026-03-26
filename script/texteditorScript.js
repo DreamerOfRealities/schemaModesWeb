7389eae6afbb9b9bf1b6967a6fe20861fc5fba46
DreamerOfRealities <dreamerofrealities@proton.me>
2026-03-26 20:08:38 +0100

    Add functional load input button

--- script/texteditorScript.js
+++ script/texteditorScript.js
@@ -19,6 +19,11 @@ var aHtmlElement = /** @class */ (function () {
                 //DEB console.log("eventListener of " + this.id + " triggered with " + this.textContent)
             });
     }
+    aHtmlElement.prototype.updateHtml = function () {
+        if (this.elem)
+            this.elem.textContent = this.cont;
+        localStorage.setItem(this.id, this.cont);
+    };
     return aHtmlElement;
 }());
 function collectStorage() {
@@ -30,23 +35,70 @@ function collectStorage() {
     }
     return content;
 }
-function clearStorage() {
-    localStorage.clear();
-    location.reload();
+function populateStorage() {
+    var myDict = [];
+    var file = loadInput.files[0];
+    file.text() //resolves as a promise
+        .then(function (value) {
+        localStorage.clear();
+        //Populate dict
+        var content = value.split(": >\n"); //assumes this specific separator to be used
+        for (var n = 0; n < content.length - 1; n++) {
+            myDict.push({
+                key: content[n].split("\n").slice(-1)[0],
+                value: content[n + 1].split("\n").slice(0, -2).join("\n")
+            });
+        }
+        //Populate html
+        for (var _i = 0, myDict_1 = myDict; _i < myDict_1.length; _i++) {
+            var item = myDict_1[_i];
+            for (var _a = 0, dynText_1 = dynText; _a < dynText_1.length; _a++) {
+                var element = dynText_1[_a];
+                if (item.key === element.id) {
+                    element.cont = item.value;
+                    element.updateHtml();
+                }
+            }
+        }
+        location.reload();
+    })
+        .catch(function (error) {
+        console.log(error);
+    });
+    // Populate html
+    // console.log("hello")
+    // for (let item of myDict){
+    //     console.log(item.key)
+    //     for (let element of dynText){
+    //         console.log(item.key + " VS " + element.id)
+    //         if (item.key == element.id) element.cont = item.value; console.log("match found")
+    //     }
+    // }
+    // location.reload()
 }
 // Initialize html elements
 var titleMain = document.getElementById("titleMain");
 var descMain = document.getElementById("descMain");
 var downloadInput = document.getElementById("downloadInput");
 var clearInput = document.getElementById("clearInput");
-var textDevAdult = new aHtmlElement("textDevAdult");
-var textHappyChild = new aHtmlElement("textHappyChild");
-var textHurtChild = new aHtmlElement("textHurtChild");
-var textAngryChild = new aHtmlElement("textAngryChild");
-var textIntExp = new aHtmlElement("textIntExp");
-var textFawn = new aHtmlElement("textFawn");
-var textFlight = new aHtmlElement("textFlight");
-var textFight = new aHtmlElement("textFight");
+var loadInput = document.getElementById("loadInput");
+//const textDevAdult = new aHtmlElement("textDevAdult")
+// const textHappyChild = new aHtmlElement("textHappyChild")
+// const textHurtChild = new aHtmlElement("textHurtChild")
+// const textAngryChild = new aHtmlElement("textAngryChild")
+// const textIntExp = new aHtmlElement("textIntExp")
+// const textFawn = new aHtmlElement("textFawn")
+// const textFlight = new aHtmlElement("textFlight")
+// const textFight = new aHtmlElement("textFight")
+var dynText = [];
+dynText.push(new aHtmlElement("textDevAdult"));
+dynText.push(new aHtmlElement("textHappyChild"));
+dynText.push(new aHtmlElement("textHurtChild"));
+dynText.push(new aHtmlElement("textAngryChild"));
+dynText.push(new aHtmlElement("textIntExp"));
+dynText.push(new aHtmlElement("textFawn"));
+dynText.push(new aHtmlElement("textFlight"));
+dynText.push(new aHtmlElement("textFight"));
 // Check for webStorage support
 if (typeof (Storage) == undefined) {
     //if (!StorageManager.persist()){
@@ -58,4 +110,5 @@ if (typeof (Storage) == undefined) {
 // Populate button functionality
 downloadInput.setAttribute("href", URL.createObjectURL(new Blob([collectStorage()], { type: 'text/plain' })));
 clearInput.setAttribute("onclick", "localStorage.clear(); location.reload()");
+loadInput.setAttribute("onchange", "populateStorage()");
 //# sourceMappingURL=texteditorScript.js.map
--- script/texteditorScript.ts
+++ script/texteditorScript.ts
@@ -5,6 +5,11 @@ class aHtmlElement{
     elem: HTMLElement
     cont: string = "Schreibe hier über dich..."
 
+    updateHtml(): void{
+        if(this.elem) this.elem.textContent = this.cont
+        localStorage.setItem(this.id, this.cont)
+    }
+
     constructor(myId:string){
         this.id = myId
         this.elem = document.getElementById(this.id)
@@ -21,6 +26,11 @@ class aHtmlElement{
     }
 }
 
+interface dict {
+    key: string
+    value: string
+}
+
 function collectStorage(): string {
     let content: string = "--- #dynamic, local content for schemaModes.html\n"
     for (let key in localStorage){
@@ -30,21 +40,75 @@ function collectStorage(): string {
     return content
 }
 
+function populateStorage(): void {
+    const myDict: dict[] = []
+    const file = loadInput.files[0]
+
+    file.text() //resolves as a promise
+        .then((value) => {
+            localStorage.clear()
+            
+            //Populate dict
+            const content = value.split(": >\n") //assumes this specific separator to be used
+            for (let n = 0; n<content.length-1; n++){
+                myDict.push({
+                    key: content[n].split("\n").slice(-1)[0], 
+                    value: content[n+1].split("\n").slice(0,-2).join("\n")
+                })
+            }
+
+            //Populate html
+            for (let item of myDict){
+                for (let element of dynText){
+                    if (item.key === element.id) {
+                        element.cont = item.value
+                        element.updateHtml()    
+                    }
+                }
+            }
+            location.reload()
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
+    // Populate html
+    // console.log("hello")
+    // for (let item of myDict){
+    //     console.log(item.key)
+    //     for (let element of dynText){
+    //         console.log(item.key + " VS " + element.id)
+    //         if (item.key == element.id) element.cont = item.value; console.log("match found")
+    //     }
+    // }
+    // location.reload()
+}
 
 // Initialize html elements
 const titleMain = document.getElementById("titleMain")
 const descMain = document.getElementById("descMain")
 const downloadInput = document.getElementById("downloadInput")
 const clearInput = document.getElementById("clearInput")
+const loadInput = <HTMLInputElement> document.getElementById("loadInput")
 
-const textDevAdult = new aHtmlElement("textDevAdult")
-const textHappyChild = new aHtmlElement("textHappyChild")
-const textHurtChild = new aHtmlElement("textHurtChild")
-const textAngryChild = new aHtmlElement("textAngryChild")
-const textIntExp = new aHtmlElement("textIntExp")
-const textFawn = new aHtmlElement("textFawn")
-const textFlight = new aHtmlElement("textFlight")
-const textFight = new aHtmlElement("textFight")
+//const textDevAdult = new aHtmlElement("textDevAdult")
+// const textHappyChild = new aHtmlElement("textHappyChild")
+// const textHurtChild = new aHtmlElement("textHurtChild")
+// const textAngryChild = new aHtmlElement("textAngryChild")
+// const textIntExp = new aHtmlElement("textIntExp")
+// const textFawn = new aHtmlElement("textFawn")
+// const textFlight = new aHtmlElement("textFlight")
+// const textFight = new aHtmlElement("textFight")
+
+const dynText: aHtmlElement[] = []
+dynText.push(new aHtmlElement("textDevAdult"))
+dynText.push(new aHtmlElement("textHappyChild"))
+dynText.push(new aHtmlElement("textHurtChild"))
+dynText.push(new aHtmlElement("textAngryChild"))
+dynText.push(new aHtmlElement("textIntExp"))
+dynText.push(new aHtmlElement("textFawn"))
+dynText.push(new aHtmlElement("textFlight"))
+dynText.push(new aHtmlElement("textFight"))
 
 
 // Check for webStorage support
@@ -58,3 +122,4 @@ if (typeof(Storage)==undefined){
 // Populate button functionality
 downloadInput.setAttribute("href", URL.createObjectURL(new Blob([collectStorage()], { type: 'text/plain' })))
 clearInput.setAttribute("onclick", "localStorage.clear(); location.reload()")
+loadInput.setAttribute("onchange", "populateStorage()")
